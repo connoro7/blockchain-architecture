9c4154fabc4fb6946b0636448179b3a6f9341993
Connor Dillon <connor@codewithconnor.com>
2021-05-03 15:05:33 -0700

    :sparkles: Implemented blockchain genesis & moving up/down chain

--- index.ts
+++ index.ts
@@ -25,5 +25,24 @@ class Block {
         hash.update(str).end()
         return hash.digest('hex')    
     }
-    
+
+}
+
+class Chain {
+    public static instance = new Chain() // Singleton
+
+    chain: Block[]
+
+    constructor() {
+        this.chain = [new Block(null, new Transaction(100, 'genesis', 'satoshi'))]
+    }
+
+    get lastBlock() {
+        return this.chain[this.chain.length -1]
+   }
+
+   addBlock(transaction: Transaction, senderPublicKey: string, signature: Buffer) {
+       const newBlock = new Block(this.lastBlock.hash, transaction)
+       this.chain.push(newBlock)
+   }
 }
